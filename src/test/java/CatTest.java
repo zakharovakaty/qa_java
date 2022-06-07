e5914f4d32fd7cd30c2a145c30f82e26965ba018
Ekaterina Zakharova <ekaterina.zakharova@t-systems.com>
2022-06-07 22:48:15 +0300

    Added unit tests to cover Cat, Lion, Feline classes

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,20 @@
+import com.example.Cat;
+import com.example.Feline;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CatTest {
+
+    Feline feline = new Feline();
+    Cat cat = new Cat(feline);
+
+    @Test
+    public void testGetSound(){
+        String actual = cat.getSound();
+        String expected = "Мяу";
+        assertEquals(expected, actual);
+    }
+
+}
